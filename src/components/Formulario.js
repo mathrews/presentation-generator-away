e8431bf0a7abb9e53953c11eab14cc183d1db531
Guilherme Araujo <guiaszul@gmail.com>
2024-05-14 15:39:09 -0300

    Update autocompletefields

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,7 +3,7 @@ import InputField from './InputField';
 import TextAreaField from './TextAreaField';
 import Button from './Button';
 import './Formulario.css';
-import { FaGlobe, FaUsers, FaLanguage, FaPrayingHands, FaPeopleCarry, FaInfoCircle, FaCross, FaQuestionCircle, FaChurch, FaHandshake, FaMapMarkedAlt, FaMapPin, FaMapSigns, FaBible } from 'react-icons/fa';
+import { FaUsers, FaMapMarkedAlt, FaMapPin, FaMapSigns, FaPeopleCarry, FaLanguage, FaPrayingHands, FaChurch, FaBible, FaInfoCircle, FaQuestionCircle, FaCross, FaHandshake } from 'react-icons/fa';
 import gerarApresentacao from '../utils/gerarApresentacao';
 
 function Formulario() {
@@ -25,6 +25,8 @@ function Formulario() {
     emQueAcreditam: '',
     intercessao: ''
   });
+
+  const [generatedFormData, setGeneratedFormData] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -56,6 +58,7 @@ function Formulario() {
   };
 
   const handleGenerate = () => {
+    setGeneratedFormData(formData);
     gerarApresentacao(formData);
   };
 
@@ -68,28 +71,28 @@ function Formulario() {
     <div className="form-container">
       <h2 className="form-title"><FaUsers className="form-icon" /> Formulário de Povo</h2>
       <form onSubmit={handleSubmit}>
-        <InputField label={<><FaUsers className="form-icon" /> Nome do povo</>} name="nomeDoPovo" icon={<FaUsers />} value={formData.nomeDoPovo} onChange={handleChange} />
+        <InputField label={<><FaUsers className="form-icon" /> Nome do povo</>} name="nomeDoPovo" icon={<FaUsers />} value={formData.nomeDoPovo} onChange={handleChange} autoComplete={generatedFormData?.nomeDoPovo} />
         <div className="input-row">
-          <InputField label={<><FaMapMarkedAlt className="form-icon" /> País</>} name="pais" icon={<FaMapMarkedAlt />} value={formData.pais} onChange={handleChange} small />
-          <InputField label={<><FaMapPin className="form-icon" /> Continente</>} name="continente" icon={<FaMapPin />} value={formData.continente} onChange={handleChange} small />
+          <InputField label={<><FaMapMarkedAlt className="form-icon" /> País</>} name="pais" icon={<FaMapMarkedAlt />} value={formData.pais} onChange={handleChange} autoComplete={generatedFormData?.pais} small />
+          <InputField label={<><FaMapPin className="form-icon" /> Continente</>} name="continente" icon={<FaMapPin />} value={formData.continente} onChange={handleChange} autoComplete={generatedFormData?.continente} small />
         </div>
-        <InputField label={<><FaMapSigns className="form-icon" /> Onde vivem</>} name="ondeVivem" icon={<FaMapSigns />} value={formData.ondeVivem} onChange={handleChange} />
-        <InputField label={<><FaPeopleCarry className="form-icon" /> População</>} name="populacao" icon={<FaPeopleCarry />} value={formData.populacao} onChange={handleChange} />
-        <InputField label={<><FaLanguage className="form-icon" /> Idioma e tradução</>} name="idiomaETraducao" icon={<FaLanguage />} value={formData.idiomaETraducao} onChange={handleChange} />
-        <InputField label={<><FaPrayingHands className="form-icon" /> Religião</>} name="religiao" icon={<FaPrayingHands />} value={formData.religiao} onChange={handleChange} />
-        <InputField label={<><FaChurch className="form-icon" /> Relação com o cristianismo</>} name="relacaoComOCristianismo" icon={<FaChurch />} value={formData.relacaoComOCristianismo} onChange={handleChange} />
+        <InputField label={<><FaMapSigns className="form-icon" /> Onde vivem</>} name="ondeVivem" icon={<FaMapSigns />} value={formData.ondeVivem} onChange={handleChange} autoComplete={generatedFormData?.ondeVivem} />
+        <InputField label={<><FaPeopleCarry className="form-icon" /> População</>} name="populacao" icon={<FaPeopleCarry />} value={formData.populacao} onChange={handleChange} autoComplete={generatedFormData?.populacao} />
+        <InputField label={<><FaLanguage className="form-icon" /> Idioma e tradução</>} name="idiomaETraducao" icon={<FaLanguage />} value={formData.idiomaETraducao} onChange={handleChange} autoComplete={generatedFormData?.idiomaETraducao} />
+        <InputField label={<><FaPrayingHands className="form-icon" /> Religião</>} name="religiao" icon={<FaPrayingHands />} value={formData.religiao} onChange={handleChange} autoComplete={generatedFormData?.religiao} />
+        <InputField label={<><FaChurch className="form-icon" /> Relação com o cristianismo</>} name="relacaoComOCristianismo" icon={<FaChurch />} value={formData.relacaoComOCristianismo} onChange={handleChange} autoComplete={generatedFormData?.relacaoComOCristianismo} />
         <div className="input-row">
-          <InputField label={<><FaBible className="form-icon" /> Cristãos no Brasil</>} name="cristaosNoBrasil" icon={<FaBible />} value={formData.cristaosNoBrasil} onChange={handleChange} />
-          <InputField label={<><FaBible className="form-icon" /> Evangélicos no Brasil</>} name="evangelicosNoBrasil" icon={<FaBible />} value={formData.evangelicosNoBrasil} onChange={handleChange} />
+          <InputField label={<><FaBible className="form-icon" /> Cristãos no Brasil</>} name="cristaosNoBrasil" icon={<FaBible />} value={formData.cristaosNoBrasil} onChange={handleChange} autoComplete={generatedFormData?.cristaosNoBrasil} />
+          <InputField label={<><FaBible className="form-icon" /> Evangélicos no Brasil</>} name="evangelicosNoBrasil" icon={<FaBible />} value={formData.evangelicosNoBrasil} onChange={handleChange} autoComplete={generatedFormData?.evangelicosNoBrasil} />
         </div>
         <div className="input-row">
-          <InputField label={<><FaBible className="form-icon" /> Cristãos pelo mundo</>} name="cristaosPeloMundo" icon={<FaBible />} value={formData.cristaosPeloMundo} onChange={handleChange} />
-          <InputField label={<><FaBible className="form-icon" /> Evangélicos pelo mundo</>} name="evangelicosPeloMundo" icon={<FaBible />} value={formData.evangelicosPeloMundo} onChange={handleChange} />
+          <InputField label={<><FaBible className="form-icon" /> Cristãos pelo mundo</>} name="cristaosPeloMundo" icon={<FaBible />} value={formData.cristaosPeloMundo} onChange={handleChange} autoComplete={generatedFormData?.cristaosPeloMundo} />
+          <InputField label={<><FaBible className="form-icon" /> Evangélicos pelo mundo</>} name="evangelicosPeloMundo" icon={<FaBible />} value={formData.evangelicosPeloMundo} onChange={handleChange} autoComplete={generatedFormData?.evangelicosPeloMundo} />
         </div>
-        <TextAreaField label={<><FaInfoCircle className="form-icon" /> Introdução</>} name="introducao" placeholder="Introdução..." value={formData.introducao} onChange={handleChange} />
-        <TextAreaField label={<><FaQuestionCircle className="form-icon" /> Como vivem</>} name="comoVivem" placeholder="Como vivem..." value={formData.comoVivem} onChange={handleChange} />
-        <TextAreaField label={<><FaCross className="form-icon" /> Em que acreditam</>} name="emQueAcreditam" placeholder="Em que acreditam..." value={formData.emQueAcreditam} onChange={handleChange} />
-        <TextAreaField label={<><FaHandshake className="form-icon" /> Intercessão</>} name="intercessao" placeholder="Intercessão..." value={formData.intercessao} onChange={handleChange} />
+        <TextAreaField label={<><FaInfoCircle className="form-icon" /> Introdução</>} name="introducao" placeholder="Introdução..." value={formData.introducao} onChange={handleChange} autoComplete={generatedFormData?.introducao} />
+        <TextAreaField label={<><FaQuestionCircle className="form-icon" /> Como vivem</>} name="comoVivem" placeholder="Como vivem..." value={formData.comoVivem} onChange={handleChange} autoComplete={generatedFormData?.comoVivem} />
+        <TextAreaField label={<><FaCross className="form-icon" /> Em que acreditam</>} name="emQueAcreditam" placeholder="Em que acreditam..." value={formData.emQueAcreditam} onChange={handleChange} autoComplete={generatedFormData?.emQueAcreditam} />
+        <TextAreaField label={<><FaHandshake className="form-icon" /> Intercessão</>} name="intercessao" placeholder="Intercessão..." value={formData.intercessao} onChange={handleChange} autoComplete={generatedFormData?.intercessao} />
         <div className="button-container">
           <Button type="button" className="clear-button" onClick={handleClear}>Limpar</Button>
           <Button type="button" className="generate-button" onClick={handleGenerate}>Gerar</Button>
