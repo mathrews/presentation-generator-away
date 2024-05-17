0d64bf245aaa1e2913e51bcfdbcf7dc9f354a669
Guilherme Araujo <guiaszul@gmail.com>
2024-05-17 20:34:09 -0300

    Adjust text titles sizes

--- src/utils/gerarApresentacao.js
+++ src/utils/gerarApresentacao.js
@@ -12,17 +12,32 @@ const gerarApresentacao = (apresentacao) => {
   // Adiciona o texto do campo "nome do povo" em uppercase no centro do slide
   const nomeDoPovoText = apresentacao.nomeDoPovo.toUpperCase(); // Transforma o texto em maiúsculas
   const nomeDoPovoTextOptions = {
-    x: "33.5%", // Posição X no centro do slide
+    x: "0%", // Posição X no centro do slide
     y: "-10%", // Posição Y no centro do slide
-    w: "33%", // Largura igual à largura do slide
+    w: "100%", // Largura igual à largura do slide
     h: "100%", // Altura igual à altura do slide
-    align: pptxgen.TextAlignCenter, // Alinhamento centralizado
+    align: "center", // Alinhamento centralizado
     fontSize: 80, // Tamanho da fonte
     fontFace: "Calibri", // Tipo de fonte
     color: "FFFFFF", // Cor do texto (branco)
     bold: true // Negrito
   };
   slide.addText(nomeDoPovoText, nomeDoPovoTextOptions);
+
+  // Adiciona o texto "País - Continente" logo abaixo do título
+  const paisContinenteText = `${apresentacao.pais} - ${apresentacao.continente}`;
+  const paisContinenteTextOptions = {
+    x: "0%", // Posição X no centro do slide
+    y: "10%", // Posição Y no centro do slide
+    w: "100%%", // Largura igual à largura do slide
+    h: "100%", // Altura igual à altura do slide
+    align: "center", // Alinhamento centralizado
+    fontSize: 40, // Tamanho da fonte
+    fontFace: "Calibri", // Tipo de fonte
+    color: "FFFFFF", // Cor do texto (branco)
+    bold: true // Negrito
+  };
+  slide.addText(paisContinenteText, paisContinenteTextOptions);
 
   // Salva a apresentação como arquivo PPTX
   pres.writeFile({
