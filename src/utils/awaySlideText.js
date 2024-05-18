a4fb1e46c2a064b436482e8edb4c959098897aec
Guilherme Araujo <guiaszul@gmail.com>
2024-05-18 01:14:19 -0300

    Add awaySlideText to make a way to add slide more easy

---
+++ src/utils/awaySlideText.js
@@ -0,0 +1,30 @@
+const addSlideText = (pptxgen, slide, title, text) => {
+    // Adiciona o título ao slide
+    slide.addText(title, {
+      x: "1.6%", 
+      y: "27%", 
+      w: "99%", 
+      h: "0%", 
+      align: "left", 
+      fontSize: 40, 
+      fontFace: "Calibri", 
+      color: "FFFFFF", 
+      bold: true 
+    });
+  
+    // Adiciona o texto ao slide
+    slide.addText(text, {
+      x: "1.6%", 
+      y: "32%", 
+      w: "99%", 
+      h: "70%", 
+      align: "left", 
+      valign: "top",
+      fontSize: 25, 
+      fontFace: "Calibri", 
+      color: "FFFFFF"
+    });
+  };
+  
+  export default addSlideText;
+  
